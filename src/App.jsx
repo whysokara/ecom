83d9bed93e9f6358c6fe5802283a17ce44b2ee52
Kara <kara_himanshu@yahoo.com>
2021-09-22 22:27:36 +0530

    Active Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
+// import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Cart />;
+  return <Home />;
 };
 
 export default App;
