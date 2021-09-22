48ee85e166be09afedcf276d9d542a1bfff7125d
Kara <kara_himanshu@yahoo.com>
2021-09-22 20:22:19 +0530

    Register/Login Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import Home from "./pages/Home";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
 // import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
