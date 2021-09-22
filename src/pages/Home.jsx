cc7005214228fd7d23193581b80e8788afab878a
Kara <kara_himanshu@yahoo.com>
2021-09-22 17:21:10 +0530

    Newsletter Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Slider from "../components/Slider";
 import Navbar from "../components/Navbar";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   );
 };
